414ebcb8f600336573d941347e045adcbaceb8d3
igor <anapa.igor@yandex.ru>
2020-09-29 18:07:35 +0300

    добавил модуль с текстовой базой данных

--- bot.py
+++ bot.py
@@ -5,20 +5,22 @@ from promotion_parser import obj_text
 from restaurant_parser import restaurant_obj_text
 from calendar_parser import calendar_obj_text
 from news_parser import news_obj_text
+from text_data import hello_list
 
 bot = telebot.TeleBot(TOKEN)
 
 
 def keyboard_menu():
-    keyboard1 = telebot.types.ReplyKeyboardMarkup()
+    keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
     keyboard1.row('Новости')
-    keyboard1.row('Акции и скидки', 'Бронирование', 'Мероприятия')
-    keyboard1.row('Трансфер', 'Рестораны и бары', 'Контакты')
+    keyboard1.row('Акции и скидки', 'Рестораны и бары')
+    keyboard1.row('Трансфер', 'Контакты', 'Бронирование',)
+    keyboard1.row('Мероприятия')
     return keyboard1
 
 
 def transfer_menu():
-    transfer_keyboard = telebot.types.ReplyKeyboardMarkup()
+    transfer_keyboard = telebot.types.ReplyKeyboardMarkup(True, True)
     transfer_keyboard.row('Бизнес', 'Премиум', 'Минивен')
     transfer_keyboard.row(('Назад'))
     return transfer_keyboard
@@ -28,14 +30,14 @@ main_keyboard = keyboard_menu()
 transfer_keyboard = transfer_menu()
 
 
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=main_keyboard)
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Что бы начать работу с ботом, нажмите меню.", reply_markup=main_keyboard)
 
 
 @bot.message_handler(content_types=['text'])
 def inline_key(message):
-    if message.text.lower() == 'привет':
+    if message.text.lower() in hello_list:
         bot.send_message(message.chat.id, 'Вас приветствует ифно-бот Alean Family Resort & SPA Doville! '
                                           'Что бы продолжить, нажмите меню.', reply_markup=main_keyboard)
 
