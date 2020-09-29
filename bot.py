27a0208c67fc6bc0430cd906de8342b6f08b325b
igor <anapa.igor@yandex.ru>
2020-09-29 20:48:33 +0300

    добавил меню в Рестораны и бары

--- bot.py
+++ bot.py
@@ -1,16 +1,15 @@
 import telebot
-from telebot import types
 from settings import TOKEN
 from promotion_parser import obj_text
 from restaurant_parser import restaurant_obj_text
 from calendar_parser import calendar_obj_text
 from news_parser import news_obj_text
-from text_data import hello_list
+from text_data import hello_list, gooby_list
 
 bot = telebot.TeleBot(TOKEN)
 
 
-def keyboard_menu():
+def main_keyboard():
     keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
     keyboard1.row('Новости')
     keyboard1.row('Акции и скидки', 'Рестораны и бары')
@@ -19,29 +18,65 @@ def keyboard_menu():
     return keyboard1
 
 
-def transfer_menu():
-    transfer_keyboard = telebot.types.ReplyKeyboardMarkup(True, True)
-    transfer_keyboard.row('Бизнес', 'Премиум', 'Минивен')
-    transfer_keyboard.row(('Назад'))
-    return transfer_keyboard
+def transfer_keyboard():
+    keyboard = telebot.types.ReplyKeyboardMarkup(True, True)
+    keyboard.row('Бизнес', 'Премиум', 'Минивен')
+    keyboard.row(('Назад'))
+    return keyboard
 
 
-main_keyboard = keyboard_menu()
-transfer_keyboard = transfer_menu()
+def restarurant_keyboard():
+    keyboard = telebot.types.ReplyKeyboardMarkup(True, True)
+    keyboard.row('Рестораны', 'Бары', 'Детям')
+    keyboard.row(('Назад'))
+    return keyboard
 
 
-@bot.message_handler(commands=['start', 'help'])
+def back_keyboard():
+    keyboard = telebot.types.ReplyKeyboardMarkup(True, True)
+    keyboard.row(('Назад'))
+    return keyboard
+
+
+main_keyboard = main_keyboard()
+transfer_keyboard = transfer_keyboard()
+restaurant_keyboard = restarurant_keyboard()
+back_keyboard = back_keyboard()
+
+
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, "Что бы начать работу с ботом, нажмите меню.", reply_markup=main_keyboard)
 
 
+@bot.message_handler(commands=['awards'])
+def help_command(message):
+    keyboard = telebot.types.InlineKeyboardMarkup()
+    keyboard.add(telebot.types.InlineKeyboardButton(
+        'Проголосовать', url='https://www.luxuryhotelawards.com/hotel/alean-family-resort-spa-doville/'))
+    bot.send_message(
+        message.chat.id,
+        'Второй год подряд пятизвездочный курорт Alean Family Resort & Spa Doville '
+        'номинирован на международную премию World Luxury Hotel Awards. \n' +
+        'Поддержать нас очень просто: \n' +
+        '1) Перейдите по ссылке премии в модуль голосования.\n' +
+        '2) Во второй строке выберите одну из трех номинаций. \n' +
+        '3) Введите свой электронный адрес. \n' +
+        '4) Нажмите кнопку Vote for this hotel. \n', reply_markup=keyboard)
+
+
 @bot.message_handler(content_types=['text'])
 def inline_key(message):
-    if message.text.lower() in hello_list:
-        bot.send_message(message.chat.id, 'Вас приветствует ифно-бот Alean Family Resort & SPA Doville! '
-                                          'Что бы продолжить, нажмите меню.', reply_markup=main_keyboard)
 
-    elif message.text.lower() == 'пока':
+    if message.text.lower() in hello_list:
+        bot.send_message(message.chat.id,
+                         'Вас приветствует ифно-бот Alean Family Resort & SPA Doville! '
+                         'Что бы продолжить, нажмите меню. \n\n'
+                         '🎉 Поддержите нас в номинации международной премии World Luxury Hotel Awards!!! 🎉 \n'
+                         'Для этого нажмите /awards',
+                         reply_markup=main_keyboard)
+
+    elif message.text.lower() in gooby_list:
         bot.send_message(message.chat.id, 'До новых встреч!')
 
     elif message.text.lower() == 'назад':
@@ -51,7 +86,16 @@ def inline_key(message):
         bot.send_message(message.chat.id, news_obj_text)
 
     elif message.text.lower() == 'рестораны и бары':
-        bot.send_message(message.chat.id, restaurant_obj_text)
+        bot.send_message(message.chat.id, 'Рестораны и бары', reply_markup=restaurant_keyboard)
+
+    elif message.text.lower() == 'рестораны':
+        bot.send_message(message.chat.id, restaurant_obj_text[:375], reply_markup=restaurant_keyboard)
+
+    elif message.text.lower() == 'бары':
+        bot.send_message(message.chat.id, 'Бары', reply_markup=restaurant_keyboard)
+
+    elif message.text.lower() == 'детям':
+        bot.send_message(message.chat.id, 'Детям', reply_markup=restaurant_keyboard)
 
     elif message.text.lower() == 'акции и скидки':
         bot.send_message(message.chat.id, obj_text)
