71a3015ddb12a38557bbdd0cd866ef3633c0f254
igor <anapa.igor@yandex.ru>
2020-11-14 11:25:17 +0300

    добавлено отображение времени работы точек питания

--- parsers/restaurant_parser.py
+++ parsers/restaurant_parser.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import fake_useragent
+from rest_period_parser import time_dict
 
 HOST = 'https://dovilleresort.ru'
 URL = 'https://dovilleresort.ru/restaurant/'
@@ -38,10 +39,12 @@ def get_content(html):
 def get_text_restaurant_parser(items):
     block_content = ['Снек-бар Bon appetit', 'Снек-бар на пляже Le Paradis', 'Детское кафе Карамелька', ]
     acc = []
-    for pos in items:
-        if pos['title'] not in block_content:
-            description = pos['desc'].split(sep='.')
-            acc.append(f"{pos['title'].upper()}\n{pos['period']}\n{description[0]}\n{pos['href']}\n")
+    for name, time in time_dict.items():
+        for pos in items:
+            if pos['title'] not in block_content:
+                if pos['title'].upper() == name:
+                    description = pos['desc'].split(sep='.')
+                    acc.append(f"{pos['title'].upper()}\n{pos['period']}\n{time}\n{description[0]}\n{pos['href']}\n")
     # print(acc)
     acc_string = '\n'.join(acc)
     return acc_string
@@ -50,6 +53,6 @@ def get_text_restaurant_parser(items):
 html = get_html(URL)
 items = get_content(html.text)
 restaurant_obj_text = get_text_restaurant_parser(items)
-print(restaurant_obj_text)
+# print(restaurant_obj_text)
 
 
