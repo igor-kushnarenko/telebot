133b5149f5b9663a73d6b917881cd451367f348d
igor <anapa.igor@yandex.ru>
2021-02-24 18:45:05 +0300

    - добавил ежедневное расписание;

--- parsers/schedule_img.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def schedule_open_img(name_image):
-    directory = '/home/maxim/PycharmProjects/telebot/static/img'
-    files = os.listdir(directory)
-    if name_image in files:
-        path_img = directory + '/' + name_image
-        return path_img
---
+++ parsers/schedule_parser.py
@@ -0,0 +1,26 @@
+import os
+
+from text_data import text_data
+
+
+def schedule_open_img(name_image):
+    directory = '/home/maxim/PycharmProjects/telebot/static/img'
+    files = os.listdir(directory)
+    if name_image in files:
+        path_img = directory + '/' + name_image
+        return path_img
+
+
+def schedule_day(text_data):
+    acc = []
+    for key, value in text_data['Дневные мероприятия'].items():
+        for pos in value:
+            time = pos[0]
+            desc = pos[1]
+            point = pos[2]
+            acc.append(f'{time} | {desc.upper()}\n{point}\n')
+    acc_string = '\n'.join(acc)
+    return acc_string
+
+
+schedule_day_parser = schedule_day(text_data)
