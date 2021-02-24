c1a043a7cc11113cf6a25a58f25edc95502acc77
igor <anapa.igor@yandex.ru>
2021-02-24 17:08:07 +0300

    - PEP8 редакция;

--- parsers/schedule_img.py
+++ parsers/schedule_img.py
@@ -1,6 +1,4 @@
 import os
-import random
-name_image = 'media_studio.jpg'
 
 
 def schedule_open_img(name_image):
@@ -9,19 +7,3 @@ def schedule_open_img(name_image):
     if name_image in files:
         path_img = directory + '/' + name_image
         return path_img
-
-
-# img = schedule_open_img(name_image)
-# print(img)
-
-
-
-
-# def media_class_open_img(name_image):
-#     directory = 'static/img'
-#     import_img_path = directory + '/' + name_image
-#     files = os.listdir(directory)
-#     for file in files:
-#         if file == name_image:
-#             path = directory + '/' + file
-#             return path
